58b5275a4b12b017388a34d80c6f3823f60db011
Winnguyen1511 <48133474+Winnguyen1511@users.noreply.github.com>
2019-04-03 17:44:07 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()
     int array[10]={2,4,3,7,1,3,6,9,2,1};
     maximumArray(array,10, 4);
     //printf("%d",array[3]);
+    //this line is my line : KHoa
     return 0;
 }
 void maximumArray(int array[],int kichthuoc, int max)
