a2a6aa8664eac8291240882b5d94d38c55d2c26b
HuyNguyen99 <48156395+HuyNguyen99@users.noreply.github.com>
2019-04-03 17:42:33 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void maximumArray(int array[],int kichthuoc, int max);
+int main()
+{
+    int array[10]={2,4,3,7,1,3,6,9,2,1};
+    maximumArray(array,10, 4);
+    //printf("%d",array[3]);
+    return 0;
+}
+void maximumArray(int array[],int kichthuoc, int max)
+{
+    int i;
+    int a;
+    for(i=0;i<kichthuoc;i++)
+    {
+        a=array[i];
+        if(a>max)
+        {
+            array[i]=0;
+        }
+        a=array[i];
+        printf("%d",a);
+    }
+}
